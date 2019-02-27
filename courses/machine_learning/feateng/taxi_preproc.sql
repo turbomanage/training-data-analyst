af84aff3aed7e8df816c69a1fa887e3b7fe11f14
David Chandler <dchandler@turbomanage.com>
2019-02-27 15:40:57 +0000

    added taxi_preproc.sql to create data files

---
+++ courses/machine_learning/feateng/taxi_preproc.sql
@@ -0,0 +1,32 @@
+# This query does the same preprocessing as the Dataflow job
+# in the feateng notebook. Run it then export the results
+# to a CSV file. Change the divisor and remainder in the MOD
+# function to get training (0) or validation (1) data for 
+# EVERY_N rows.
+
+CREATE OR REPLACE TABLE `cpb100-151023.taxifare.valid_200k` AS
+SELECT
+  (tolls_amount + fare_amount) AS fare_amount,
+  FORMAT_DATE ("%a", DATE (pickup_datetime)) AS dayofweek,
+  EXTRACT(HOUR FROM pickup_datetime) AS hourofday,
+  pickup_longitude AS pickuplon,
+  pickup_latitude AS pickuplat,
+  dropoff_longitude AS dropofflon,
+  dropoff_latitude AS dropofflat,
+  passenger_count*1.0 AS passengers,
+  "key" as key
+FROM
+  `nyc-tlc.yellow.trips` 
+WHERE
+  trip_distance > 0
+  AND fare_amount >= 2.5
+  AND pickup_longitude > -78
+  AND pickup_longitude < -70
+  AND dropoff_longitude > -78
+  AND dropoff_longitude < -70
+  AND pickup_latitude > 37
+  AND pickup_latitude < 45
+  AND dropoff_latitude > 37
+  AND dropoff_latitude < 45
+  AND passenger_count > 0
+  AND MOD(ABS(FARM_FINGERPRINT(STRING(pickup_datetime))), 5000) = 0
